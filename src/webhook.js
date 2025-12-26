7b0fcd4781bdc5b6809f0ddab60eccf7589c511c
Ruchit Sonawane <sonawaneruchit@gmail.com>
2025-12-26 08:07:44 +0530

    Add headers to webhook request

--- src/webhook.js
+++ src/webhook.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
 export async function sendWebhook(payload) {
-  await axios.post(process.env.LOVABLE_WEBHOOK_URL, payload);
+  await axios.post(
+    process.env.LOVABLE_WEBHOOK_URL,
+    payload,
+    { headers: { "Content-Type": "application/json" } }
+  );
 }
